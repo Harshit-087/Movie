ec47024f58147b54a4cfc2fbb4414b140b16425e
Harshit-087 <harshit283773@gmail.com>
2025-07-28 08:08:48 +0530

    adding server files

---
+++ server/route/router.js
@@ -0,0 +1,64 @@
+import express  from "express";
+import dotenv from"dotenv"
+import cors from "cors"
+import axios from "axios"
+dotenv.config()
+
+const router = express.Router();
+
+
+router.use(cors())
+router.get("/movie",async(req,res)=>{
+    const title = req.query.t;
+    const result=await axios.get(`http://www.omdbapi.com/?apikey=${process.env.MOVIE_API_KEY}&t=${title}`);
+    const data =  result.data.results;
+    console.log("backend movie title:",data)
+    res.status(200).json(data);
+
+})
+router.get("/video", async (req, res) => {
+  const params = req.query.video_id;
+  const result = await fetch(`https://vidsrc.net/embed/${params}`);
+  console.log("aaaa:", result);
+
+  res.status(200).json({ data: result.url }); // ⚠️ result.url is NOT valid
+});
+
+router.get("/nowplayed",async(req,res)=>{
+  // const value = req.query
+  //  console.log("apikey:",process.env.TMDB_API_KEY)
+  const result=await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`);
+        const data= result.data.results;
+        console.log("NowPlaying data:",data);
+        res.status(200).json(data)
+
+})
+
+
+router.get("/toprated",async(req,res)=>{
+  // const value=req.query
+  // console.log("apikey:",process.env.TMDB_API_KEY)
+  const result = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=5`)
+        const data =  result.data.results;
+        console.log("top rated data :",data)
+           res.status(200).json(data)
+})
+
+router.get("/popular",async(req,res)=>{
+  // const value=req.query
+  //  console.log("apikey:",process.env.TMDB_API_KEY)
+   const popularResult=await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=2`);
+        const data= popularResult.data.results;
+        console.log("popular data:",data);
+         res.status(200).json(data)
+})
+
+router.get("/upcoming",async(req,res)=>{
+  // const value = req.query
+  //  console.log("apikey:",process.env.TMDB_API_KEY)
+      const upcomingResult=await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=3`);
+        const data= upcomingResult.data.results;
+        console.log("upcoming data:",data);
+           res.status(200).json(data)
+})
+export default router;
