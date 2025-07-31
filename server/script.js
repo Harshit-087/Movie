02ce3b92979663f6b4b15d68964c4808fb6b5b96
Harshit-087 <harshit283773@gmail.com>
2025-07-31 06:09:06 +0530

    package\

--- server/script.js
+++ server/script.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router from "./route/router.js"
-import connectionDB from "../../db/connection.js"
+import connectionDB from "../db/connection.js"
 
 const app =express();
 connectionDB();
